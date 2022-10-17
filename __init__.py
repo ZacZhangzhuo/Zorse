7977b568c39dc38448af3d811e9aaeab5437ccd0
ZacZhangzhuo <zac.zhangzhuo@gmail.com>
2022-10-17 10:07:13 +0200

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
 
-# """
-# Zorses are hybrid animals, created by cross-breeding a zebra and a horse. 
-# Zorses are typically larger than zebras, with wider bodies and longer legs. 
-# Zorses have the striped coat of a zebra, but their stripes are usually not as pronounced. 
-# Zorses are also more intelligent than zebras, and they are able to remember complex commands. 
-# Zorses make good riding animals, and they are also used for draught work. 
-# Zorses are very strong, and they can easily pull heavy loads. 
-# Zorses are also very fast, and they can reach speeds of up to 40 miles per hour. 
-# Zorses are very hardy animals, and they can survive in harsh conditions. 
-# Zorses are found in Africa, Asia, and Europe.
-# """
+"""
+Zorses are hybrid animals, created by cross-breeding a zebra and a horse. 
+Zorses are typically larger than zebras, with wider bodies and longer legs. 
+Zorses have the striped coat of a zebra, but their stripes are usually not as pronounced. 
+Zorses are also more intelligent than zebras, and they are able to remember complex commands. 
+Zorses make good riding animals, and they are also used for draught work. 
+Zorses are very strong, and they can easily pull heavy loads. 
+Zorses are also very fast, and they can reach speeds of up to 40 miles per hour. 
+Zorses are very hardy animals, and they can survive in harsh conditions. 
+Zorses are found in Africa, Asia, and Europe.
+"""
 
 from .core_zVector import *
 
